a4f42b6bd262fd73c4de7e891c21cfedc0db6236
ivandariovelezvergara <ivandariovelezvergara@gmail.com>
2018-03-10 14:16:33 -0500

    Hacer cambios

---
+++ Data/Migrations/20180303215457_AddItems.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace AspNetCoreTodo.Data.Migrations
+{
+    public partial class AddItems : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // (... some code)
+
+            migrationBuilder.CreateTable(
+            name: "Items",
+            columns: table => new
+            {
+                Id = table.Column<Guid>(type: "BLOB", nullable: false),
+                DueAt = table.Column<DateTimeOffset>(type: "TEXT", nullable: true),
+                IsDone = table.Column<bool>(type: "INTEGER", nullable: false),
+                Title = table.Column<string>(type: "TEXT", nullable: true)
+            },
+            constraints: table =>
+            {
+              table.PrimaryKey("PK_Items", x => x.Id);
+            });
+
+            // (some code...)
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // (... some code)
+
+            migrationBuilder.DropTable(
+                name: "Items");
+
+            // (some code...)
+        }
+    }
+}
